4b0869920206e0e1c8a14a5763a9375d45460813
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-11-07 23:31:55 -0300

    GoBarber Web - Criando estrutura inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+import Routes from './routes';
+import history from './services/history';
+
+export default function src() {
+    return (
+        <Router history={history}>
+            <Routes />
+        </Router>
+    );
+}
