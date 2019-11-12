732e1e9485cc14085b4e60a282d5495a0e8808f8
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-11-11 23:52:50 -0300

    GoBarber Web - Finalizando Header e carregando notificações. Iniciando tela de Perfil.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-    return <div />;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit() {}
+
+    return (
+        <Container>
+            <Form initialData={profile} onSubmit={handleSubmit}>
+                <Input name="name" placeholder="Nome Completo" />
+                <Input name="email" type="email" placeholder="E-mail" />
+
+                <hr />
+                <Input
+                    type="password"
+                    name="oldPassword"
+                    placeholder="Senha atual"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Nova senha"
+                />
+                <Input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmar nova senha"
+                />
+
+                <button type="submit">Atualizar perfil</button>
+            </Form>
+            <button type="button">Sair do GoBarber</button>
+        </Container>
+    );
 }
