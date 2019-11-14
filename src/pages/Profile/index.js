735e0e17fdbf7a0470b74030bed22a9588f03275
Luiz Doreto <luizdoreto@Luizs-MacBook-Air.local>
2019-11-14 11:37:40 -0300

    GoBarber Web - Finalizando edição de perfil e logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Profile() {
     const profile = useSelector(state => state.user.profile);
+    const dispatch = useDispatch();
 
-    function handleSubmit() {}
+    function handleSubmit(data) {
+        dispatch(updateProfileRequest(data));
+    }
+
+    function handleSignOut() {
+        dispatch(signOut());
+    }
 
     return (
         <Container>
             <Form initialData={profile} onSubmit={handleSubmit}>
+                <AvatarInput name="avatar_id" />
                 <Input name="name" placeholder="Nome Completo" />
                 <Input name="email" type="email" placeholder="E-mail" />
 
@@ -34,7 +45,9 @@ export default function Profile() {
 
                 <button type="submit">Atualizar perfil</button>
             </Form>
-            <button type="button">Sair do GoBarber</button>
+            <button type="button" onClick={handleSignOut}>
+                Sair do GoBarber
+            </button>
         </Container>
     );
 }
